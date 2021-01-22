31244481505b46cd57753dee2b25d04a34836f73
tibraj <tibraj94@gmail.com>
2021-01-22 16:26:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+// document.getElementById('fetch').addEventListener('click', weather);
+
+// function weather() {
+//     fetch('https://www.openweathermap.org/api', {
+//         method: 'GET',
+//         headers : { 
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json'
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         return data
+//         });
+//     }
